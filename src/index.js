4218932965c274c7ab2dbdbbca94a34ad9ed6452
unknown <ericjiang97@gmail.com>
2017-09-04 17:08:52 +1000

    Added Push, and updated Components

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ import './index.css';
 import lightBaseTheme from "material-ui/styles/baseThemes/lightBaseTheme";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import getMuiTheme from "material-ui/styles/getMuiTheme";
+import injectTapEventPlugin from "react-tap-event-plugin";
 
+injectTapEventPlugin();
 const store = createStore(
   combineReducers(reducers),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
