e1d4cff49b8bb0b6a991e6eac3bda89313e0d081
unknown <ericjiang97@gmail.com>
2017-09-04 17:49:55 +1000

    Updated Card Components and Color Scheming

--- src/components/employee/index.js
+++ src/components/employee/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, CardHeader} from 'material-ui/Card';
+import {Card, CardHeader,CardActions} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import IconButton from 'material-ui/IconButton';
 import Toggle from 'material-ui/Toggle';
@@ -10,6 +10,7 @@ import AlertIcon from 'material-ui/svg-icons/alert/error';
 import CheckIcon from 'material-ui/svg-icons/action/check-circle';
 import CalendarIcon from 'material-ui/svg-icons/action/event';
 
+
 class EmployeeCard extends React.Component {      
     render(){
         return(
@@ -17,41 +18,39 @@ class EmployeeCard extends React.Component {
                 <CardHeader
                     title={
                         <div>
-                            <div style={{float: "left", flex: 0, order: 0}}>
+                            <div>
                                 {this.props.name}
-                            </div>
-                            
-
-                            <div style={{float: "right", flex: 0, order: 1}}>
-                                <IconButton>
-                                    <PhoneIcon />
-                                </IconButton>
-                                <IconButton>
-                                    <CalendarIcon />
-                                </IconButton>
-                                <IconButton>
-                                    {
-                                        this.props.status ?
-                                        <IconButton>
-                                            <CheckIcon color={green300}/>
-                                        </IconButton> :
-                                        <IconButton>
-                                            <AlertIcon color={red300}/>
-                                        </IconButton>
-                                        
-                                    }
-                                </IconButton>
                             </div>
                         </div> 
                     }
                     subtitle={
-                        <div style={{flex: 1, order: 1}}>
-                            {this.props.jobTitle} <br />
+                        <div>
+                            <b>Job Title:</b> {this.props.jobTitle} <br />
                             <b>Last Seen:</b> {this.props.lastSeen}
                         </div>
                     }
                     avatar={this.props.avatar}
                     />
+                <CardActions>
+                    <IconButton>
+                        <PhoneIcon />
+                    </IconButton>
+                    <IconButton>
+                        <CalendarIcon />
+                    </IconButton>
+                    <IconButton>
+                        {
+                            this.props.status ?
+                            <IconButton>
+                                <CheckIcon color={green300}/>
+                            </IconButton> :
+                            <IconButton>
+                                <AlertIcon color={red300}/>
+                            </IconButton>
+                            
+                        }
+                    </IconButton>
+                </CardActions>
           </Card>
         );
     }
--- src/components/sidebar/heading.js
+++ src/components/sidebar/heading.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const FutureYouHeading = ({ style, onTouchTap }) =>
-        <span onTouchTap={onTouchTap} style={{fontSize: "32px", textAlign: "center", padding: "0.5em", marginTop: "24px"}}>
-               eHackuate
+        <span onTouchTap={onTouchTap} style={{fontSize: "32px", textAlign: "center", marginTop: "24px"}}>
+               <img src="./images/logo.png" width="100%"/>
         </span>;
 
 export default FutureYouHeading;
